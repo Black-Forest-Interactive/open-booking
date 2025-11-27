8a3eb66633b912a01194ed10b3de2c39f0ceb295
sambalmueslie <github@sambalmueslie.de>
2025-11-27 23:10:33 +0100

    merge everything together

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,19 +50,32 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:testcontainers")
+
     // velocity
-    implementation("org.apache.velocity:velocity-engine-core:2.3")
+    implementation("org.apache.velocity:velocity-engine-core:2.4.1")
     implementation("org.apache.velocity.tools:velocity-tools-generic:3.1")
+
     // FOP
-    implementation("org.apache.xmlgraphics:fop:2.8")
+    implementation("org.apache.xmlgraphics:fop:2.11")
+    implementation("org.apache.xmlgraphics:xmlgraphics-commons:2.11")
+
+    // OpenPDF
+    implementation("com.github.librepdf:openpdf:3.0.0")
+    implementation("org.xhtmlrenderer:flying-saucer-pdf:10.0.3")
+
+    // qrcode
+    implementation("com.google.zxing:core:3.5.3")
+    implementation("com.google.zxing:javase:3.5.3")
+
     // POI
-    implementation("org.apache.poi:poi:5.2.3")
-    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    implementation("org.apache.poi:poi:5.4.1")
+    implementation("org.apache.poi:poi-ooxml:5.4.1")
     implementation("builders.dsl:spreadsheet-builder-poi:3.0.1")
+
     // mail
-    implementation("org.simplejavamail:simple-java-mail:7.9.1")
-    implementation("org.simplejavamail:batch-module:7.9.1")
-    implementation("org.simplejavamail:authenticated-socks-module:7.9.1")
+    implementation("org.simplejavamail:simple-java-mail:8.12.6")
+    implementation("org.simplejavamail:batch-module:8.12.6")
+    implementation("org.simplejavamail:authenticated-socks-module:8.12.6")
 }
 
 application {
